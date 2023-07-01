d7a2b60afa93cd5ef4ea227406d37a9b389f101b
wangqi <1722009706@qq.com>
2023-07-01 15:10:44 +0800

    make

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-/* // main.js
+// main.js
 
 // electron 模块可以用来控制应用的生命周期和创建原生浏览窗口
 const { app, BrowserWindow } = require('electron')
@@ -35,14 +35,14 @@ app.whenReady().then(() => {
 })
 
 // 除了 macOS 外，当所有窗口都被关闭的时候退出程序。 因此, 通常
-// 对应用程序和它们的菜单栏来说应该时刻保持激活状态, 
+// 对应用程序和它们的菜单栏来说应该时刻保持激活状态,
 // 直到用户使用 Cmd + Q 明确退出
 app.on('window-all-closed', () => {
     if (process.platform !== 'darwin') app.quit()
 })
 
 // 在当前文件中你可以引入所有的主进程代码
-// 也可以拆分成几个文件，然后用 require 导入。 */
+// 也可以拆分成几个文件，然后用 require 导入。
 
 
 // 在主进程中.
@@ -57,36 +57,3 @@ app.whenReady().then(() => {
     //   view.webContents.loadURL('https://www.baidu.com/')
     view.webContents.loadFile('index.html')
 }) */
-
-function addwv() {                                              // 加载BrowserView，单独写个函数，方便看代码
-    var view = new BrowserView()
-    mainWindow.setBrowserView(view)
-    view.setBounds({ x: 0, y: 50, width: 1000, height: 680 })
-    view.webContents.loadURL('https://electronjs.org')
-}
-
-const { app, BrowserView, BrowserWindow } = require('electron')
-
-app.on('ready', () => {
-    mainWindow = new BrowserWindow({/*省略一堆参数*/ })
-    mainWindow.loadFile('index.html')                         // 加载一个本地文件
-
-    // 解决方法1
-
-    mainWindow.webContents.on('did-stop-loading', () => {       // 随便找个时机，加载BrowserView(必须是墨迹的did-stop-loading,没有loadFile给did-stop-loading墨迹,该方法会失效)
-        /* 不要傻傻的直接复制，看看自己的new的BrowserWindow名字叫啥 */
-        addwv()
-    })
-
-    // 解决方法2
-    // const { setTimeout } = require('timers')                    // 写不写都行，写了时间更准
-    // setTimeout(function () {                                    // 或者这个时机，加载BrowserView
-    //     addwv()
-    // }, 10)
-    // addwv()
-
-    // 解决方法3
-    // addwv()                                                  // 在加载BrowserView的附近，小小的改动一下窗口尺寸，触发bug
-    // 使用 setFullScreen 触发 BrowserView 的正常显示，是魔法，不要动
-    // mainWindow.setFullScreen(true);mainWindow.setFullScreen(false)/* 不要傻傻的直接复制，看看自己的new的BrowserWindow名字叫啥 */
-})
